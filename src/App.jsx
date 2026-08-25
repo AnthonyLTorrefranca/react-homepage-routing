f0a5b7e107a3cdd8765895caaced2014bc917858
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-08-25 16:51:24 +0800

    init: react router definition

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom'
+import Router from './Router'
+
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/cart">Cart</Link>
+    </nav>
+      <Router /> 
     </>
   );
 }
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from 'components/Home'
-import Cart from 'components/Cart'
+import Home from './components/Home'
+import Cart from './components/Cart'
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="//components/Home" element={<Home />} />
-      <Route path="/components/Cart" element={<Cart />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
     </Routes>
   )
 }
