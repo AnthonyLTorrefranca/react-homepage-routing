c8f082babc0cfb4ed5487f13bf8733fa50c5dbd8
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-08-25 20:14:21 +0800

    feat: navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { Link } from 'react-router-dom'
-
+import NavigationBar from './components/NavigationBar.jsx'
+import Router from './Router'
 
 const App = () => {
   return (
     <>
-      <nav className="flex items-center align-middle justify-center h-10 bg-gray-500">
-        <h1>APP</h1>
-      </nav>
+      <NavigationBar />
+      <Router />
     </>
   )
 }
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import STZY from '../assets/stzy.png'
+
+export default function NavigationBar() {
+  return (
+    <>
+    <nav className="flex items-center align-middle justify-around h-10 bg-gray-500">
+        <div>
+            <img src={STZY} className="h-10 hover:cursor-pointer"></img>
+        </div>
+        <div className="mr-10">
+            <Link className="mr-10 px-2 py-2 hover:bg-gray-400" to="/">Home</Link>
+            <Link className="mr-10 px-2 py-2 hover:bg-gray-400" to="/cart">🛒</Link>
+        </div>
+    </nav>
+    </>
+  )
+}
