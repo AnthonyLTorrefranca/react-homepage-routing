415e92a235af82e4662ff7cbf1654557d49abc87
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-08-25 20:18:23 +0800

    feat: added a target blank to logo image

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom'
 import STZY from '../assets/stzy.png'
+const STZYLINK = "https://www.facebook.com/steezyapparelco"
 
 export default function NavigationBar() {
   return (
     <>
     <nav className="flex items-center align-middle justify-around h-10 bg-gray-500">
         <div>
-            <img src={STZY} className="h-10 hover:cursor-pointer"></img>
+            <a href={STZYLINK} target="blank">
+                <img src={STZY} className="h-10 hover:cursor-pointer"></img>
+            </a>
         </div>
         <div className="mr-10">
             <Link className="mr-10 px-2 py-2 hover:bg-gray-400" to="/">Home</Link>
