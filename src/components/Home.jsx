faf0fdea2799628795e956dddbad77411681dcfe
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-08-25 21:00:39 +0800

    refactor: resize and arrange the text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,13 @@ const HEROSEC = '/src/assets/hero.jpg'
 
 export default function Home() {
   return (
-    <>
-      <img src={HEROSEC} alt="Hero section" className="w-screen"></img>
-      <h1>Home</h1>
-      <p>Welcome to our clothing store!</p>
-      <p>Choose products below.</p>
-    </>
+    <div className="heroSection flex relative">
+      <img src={HEROSEC} alt="Hero section" className="w-screen z-0"></img>
+      <div className="absolute top-50 right-100">
+        <p className="text-9xl">Home</p>
+        <p className="text-4xl">Welcome to our clothing store!</p>
+        <p className="text-2xl">Choose products below.</p>
+      </div>
+    </div>
   );
 }
