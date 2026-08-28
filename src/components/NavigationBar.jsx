b1299431ab5abe4a9ffe1d3d2a2c993d8e46f2b4
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-08-28 19:38:44 +0800

    feat: inserting anchor tag in the logo image

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,12 +3,22 @@ import STZYLOGO from '../assets/stzy.png'
 
 export default function NavigationBar() {
   return (
-    <div className="flex justify-center items-center h-15 bg-black">
+    <div className="flex items-center justify-around h-15 bg-black">
       <div>
-        <img src={STZYLOGO} alt="" /></div>
+        <a 
+          href="https://www.facebook.com/steezyapparelco" 
+          target="_blank"
+          rel='noopener noreferrer'>
+          <img 
+            src={STZYLOGO} 
+            className="h-15"
+            decoding="async"
+            alt="" />
+        </a>
+      </div>
       <div>
-        <Link className="text-white hover:underline">Home</Link>
-        <Link className="text-white hover:underline">Cart</Link>
+        <Link className="mx-5 p-3 text-white  cursor-pointer hover:underline hover:bg-gray-800" to="/">Home</Link>
+        <Link className="mx-5 p-3 text-white  cursor-pointer hover:underline hover:bg-gray-800" to="/cart">Cart </Link>
       </div>
     </div>
   )
