7b64cae89ac5f8a476b0e16133f4ec6e2a196358
Anthony Torrefranca <anthony.torrefranca@gmail.com>
2026-08-29 23:04:53 +0800

    refactor: written from memory

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import {BrowserRouter} from 'react-router-dom'
 import NavigationBar from './components/NavigationBar'
-import Router from './Router'
+
 export default function App() {
   return (
-  <>
-    <NavigationBar />
-    <Router />
-  </>
+  <BrowserRouter>
+    <NavigationBar/>
+  </BrowserRouter>
   )
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,18 +5,14 @@ export default function NavigationBar() {
   return (
     <div className="flex items-center justify-around h-15 bg-black">
       <div>
-        <a href="https://www.facebook.com/steezyapparelco" 
-          target="_blank"
-          rel='noopener noreferrer'>
-          <img src={STZYLOGO} 
-            className="h-15"
-            decoding="async"
-            alt="" />
-        </a>
+        <img src={STZYLOGO} 
+          className="h-15"
+          decoding="async"
+          alt="" />
       </div>
       <div>
-        <Link className="text-white m-3 text-xl p-3 hover:underline hover:bg-gray-700" to="/">Home</Link>
-        <Link className="text-white m-3 text-xl p-3 hover:underline hover:bg-gray-700" to="/cart">Cart</Link>
+        <Link to="/">Home</Link>
+        <Link to="/cart">Cart</Link>
       </div>
     </div>
   )
